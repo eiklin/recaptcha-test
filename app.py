076cf4632d4e5a64c07cc74be145d9947e541322
WONG Eik Lin <33109489+eiklin@users.noreply.github.com>
2022-03-17 10:44:45 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
 from flask import Flask, render_template, request
-from azure.servicebus import ServiceBusClient, ServiceBusMessage
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
 
     return render_template('index.html')
 
-@app.route('/submit/', methods=['POST','GET'])
-def submit():
+@app.route('/googlee4df84b135df732d.html')
+def google():
+
+    return render_template('googlee4df84b135df732d.html')
+
+@app.route('/result/', methods=['POST','GET'])
+def result():
 
     from urllib.parse import urlencode
     from urllib.request import urlopen
     import json
 
-    URIReCaptcha = 'https://www.google.com/recaptcha/api/siteverify'
-    recaptchaResponse = request.form.get("g-recaptcha-response")
-    private_recaptcha = '6LcG9WweAAAAAOwg2iS5c_aPvD85tFZbidlwPDfr'
-    remoteip = request.remote_addr
+    uri_recaptcha = 'https://www.google.com/recaptcha/api/siteverify'
+    recaptcha_response = request.form.get("g-recaptcha-response")
+    secret_key = '6LcG9WweAAAAAOwg2iS5c_aPvD85tFZbidlwPDfr'
+    remote_ip = request.remote_addr
     params = urlencode({
-        'secret': private_recaptcha,
-        'response': recaptchaResponse,
-        'remoteip': remoteip,
+        'secret': secret_key,
+        'response': recaptcha_response,
+        'remoteip': remote_ip,
     })
 
      # print params
-    data = urlopen(URIReCaptcha, params.encode('utf-8')).read()
+    data = urlopen(uri_recaptcha, params.encode('utf-8')).read()
     result = json.loads(data)
     success = result.get('success')
     challenge_ts = result.get('challenge_ts')
     hostname = result.get('hostname')
 
-    return render_template('submit.html',success=success,challenge_ts=challenge_ts,hostname=hostname)
+    return render_template('result.html',success=success,challenge_ts=challenge_ts,hostname=hostname)
 
